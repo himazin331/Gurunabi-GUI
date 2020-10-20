7ec50aca596e75e7169e310633db6c2eb26e92e2
himazin331 <sion20022013@gmail.com>
2020-10-21 07:49:17 +0900

    back program

--- gurunabi.py
+++ gurunabi.py
@@ -3,32 +3,55 @@ import json
 
 import config
 
-import pprint
+class GurunabiAPI():
+    def __init__(self):
+        self.url = "https://api.gnavi.co.jp/RestSearchAPI/v3/" # API URL
+        self.params = {} # パラメータ
+        self.params['keyid'] = config.API_KEY # API Key
+        self.place = config.PLACE # 地域
+        self.category = config.CATEGORY # カテゴリ
+        # パラメータ名
+        self.param_name = ["lunch", "breakfast", "takeout", "delivery", "no_smoking", "private_room",
+                            "midnight", "until_morning", "bottomless_cup", "outret", "card", "e_money",
+                            "parking", "web_reserve"]
 
-def main():
-    url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+    def callAPI(self, rbv, search_word, placel, catel, ckb_exp):
 
-    params={}
-    params["keyid"] = config.API_KEY
+        self.params['hit_per_page'] = 50
 
-    params["name"] = "ピザ"
+        # 検索方式
+        if rbv == 0: # フリーワード検索
+            self.params['freeword'] = search_word
+        elif rbv == 1: # 店名検索
+            self.params['name'] = search_word
+        elif rbv == 2: # 住所検索
+            self.params['address'] = search_word
 
+        # 都道府県
+        self.params['pref'] = self.place[placel[0].get()]['placecode']
+        # エリアL
+        self.params['areacode_l'] = self.place[placel[0].get()]['areaL'][placel[1].get()]['areaLcode']
+        # エリアM
+        if "None" not in self.place[placel[0].get()]['areaL'][placel[1].get()]['areaM'][placel[2].get()]:
+            self.params['areacode_m'] = self.place[placel[0].get()]['areaL'][placel[1].get()]['areaM'][placel[2].get()]
 
-    result = requests.get(url, params)
-    result = result.json()
+        try:
+            # 大カテゴリ
+            if "None" not in self.category[catel[0].get()]['categoryL_code']:
+                self.params['category_l'] = self.category[catel[0].get()]['categoryL_code']
+                # 小カテゴリ
+                if "None" not in self.category[catel[0].get()]['categoryS'][catel[1].get()]:
+                    self.params['category_s'] = self.category[catel[0].get()]['categoryS'][catel[1].get()]
+        except KeyError:
+            pass
 
-    #pprint.pprint(result)
+        # 条件指定
+        for i in range(14):
+            if ckb_exp[i].get():
+                self.params[self.param_name[i]] = 1
 
-    print(result['rest'][0]['address'])
+        # リクエスト
+        result = requests.get(self.url, self.params)
+        result = result.json() # json整形
 
-
-    print(result['rest'][0]['name_kana'])
-
-
-    print(result['rest'][0]['code']['areaname'])
-
-
-    print(result['rest'][0]['code']['category_name_l'][:2])
-
-if __name__ == '__main__':
-    main()
+        return result
