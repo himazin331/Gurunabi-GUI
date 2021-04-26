42476677820c45cfb25a4b7d139e558bbcd2d6c0
himazin331 <sion20022013@gmail.com>
2021-04-27 02:21:50 +0900

    back program

--- gurunabi.py
+++ gurunabi.py
@@ -1,30 +1,28 @@
 import requests
-import json
-
 import config
+
 
 class GurunabiAPI():
     def __init__(self):
-        self.url = "https://api.gnavi.co.jp/RestSearchAPI/v3/" # API URL
-        self.params = {} # パラメータ
-        self.params['keyid'] = config.API_KEY # API Key
-        self.place = config.PLACE # 地域
-        self.category = config.CATEGORY # カテゴリ
+        self.url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"  # API URL
+        self.params = {}  # パラメータ
+        self.params['keyid'] = config.API_KEY  # API Key
+        self.place = config.PLACE  # 地域
+        self.category = config.CATEGORY  # カテゴリ
         # パラメータ名
         self.param_name = ["lunch", "breakfast", "takeout", "delivery", "no_smoking", "private_room",
                             "midnight", "until_morning", "bottomless_cup", "outret", "card", "e_money",
                             "parking", "web_reserve"]
 
     def callAPI(self, rbv, search_word, placel, catel, ckb_exp):
-
         self.params['hit_per_page'] = 50
 
         # 検索方式
-        if rbv == 0: # フリーワード検索
+        if rbv == 0:  # フリーワード検索
             self.params['freeword'] = search_word
-        elif rbv == 1: # 店名検索
+        elif rbv == 1:  # 店名検索
             self.params['name'] = search_word
-        elif rbv == 2: # 住所検索
+        elif rbv == 2:  # 住所検索
             self.params['address'] = search_word
 
         # 都道府県
@@ -52,6 +50,6 @@ class GurunabiAPI():
 
         # リクエスト
         result = requests.get(self.url, self.params)
-        result = result.json() # json整形
+        result = result.json()  # json整形
 
         return result
